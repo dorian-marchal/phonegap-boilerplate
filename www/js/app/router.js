0fd04452461138633854d521dbed331a28e3b065
dorian-marchal <marchal.dorian@gmail.com>
2015-02-23 22:27:00 +0100

    Supprime les éléments superflux pour les placer dans une branche

--- www/js/app/router.js
+++ www/js/app/router.js
@@ -2,44 +2,14 @@ define([
     'jquery',
     'backbone',
     'core/utils/pageslider',
-    'app/views/Layout',
-    'app/views/Home',
-    'app/views/NextPage',
-], function ($, Backbone, PageSlider, LayoutView, HomeView, NextPageView) {
+], function ($, Backbone, PageSlider) {
     'use strict';
 
     var slider = new PageSlider($('body'));
 
-    var layout = new LayoutView();
-    var homeView = new HomeView();
-    var nextPageView = new NextPageView();
-
     return Backbone.Router.extend({
 
         routes: {
-            '': 'home',
-            'nextPage': 'nextPage',
-        },
-
-        loadPage: function(view) {
-            layout.setContentView(view);
-            slider.slidePage(layout.render().$el);
-        },
-
-        home: function () {
-            homeView.delegateEvents();
-            layout.setOptions({
-                title: 'Accueil',
-            });
-            this.loadPage(homeView);
-        },
-
-        nextPage: function () {
-            nextPageView.delegateEvents();
-            layout.setOptions({
-                title: 'NextPage',
-            });
-            this.loadPage(nextPageView);
         },
 
     });
