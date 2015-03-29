e8560e8dd1d04cadbfc4512b40604c81bbb48e0d
dorian-marchal <marchal.dorian@gmail.com>
2015-03-29 23:32:11 +0200

    Add token auth handling to the ApiHelper

--- www/js/core/utils/ApiHelper.js
+++ www/js/core/utils/ApiHelper.js
@@ -13,11 +13,25 @@ define([
 
     var ApiHelper = function (serverHost, serverPort) {
 
+        /**
+         * Globally add the token to all Ajax requests
+         * @param {String} token
+         */
+        this.setToken = function(token) {
+
+            $.ajaxSetup({
+                data: {
+                    access_token: token,
+                },
+            });
+        };
+
         this._ajax = function(method, url, settings) {
             settings = settings || {};
 
             settings.method = method;
             settings.url = 'http://' + serverHost + ':' + serverPort + url;
+
             return $.ajax(settings);
         };
 
