7fa818048a5b0273028fb3aa449c5fe5cd4f93d9
Dorian MARCHAL <d.marchal@moobee.fr>
2015-03-31 16:40:20 +0200

    Makes authentification optional

--- www/js/app.js
+++ www/js/app.js
@@ -20,11 +20,19 @@ require(['config'], function(config) {
             $('head').append('<script src="http://' + config.debug.weinreHost + ':' + config.debug.weinrePort + '/target/target-script-min.js#anonymous"></script>');
         }
 
-        auth.checkLogin(function() {
-
+        var start = function() {
             router.setController(new Controller());
             router.setSlider(new PageSlider($('body')));
             Backbone.history.start();
-        });
+        };
+
+        if (config.useAuth) {
+            auth.checkLogin(function() {
+                start();
+            });
+        }
+        else {
+            start();
+        }
     });
 });
