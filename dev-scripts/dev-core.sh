99bf80c26908c02c3e716b0479b45a744745a342
dorian-marchal <marchal.dorian@gmail.com>
2015-04-05 11:27:20 +0200

    Improve pb CLI

---
+++ dev-scripts/dev-core.sh
@@ -0,0 +1,33 @@
+# Core script to include in all dev scripts
+# Exports :
+#   $pb_repo : Remote pb repo
+#   $pb_branch : Branch to pull from on $pb_repo
+
+CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# Load config file
+. "${CURRENT_DIR}/pb-config.sh"
+
+# Get pb repo from git pb.repo config or ask the user if not set
+
+if [ -z "$(git config pb.repo)" ]; then
+
+    read -p "Enter core git repository (default: '$default_pb_repo'): " pb_repo; \
+
+    if [[ -z "$pb_repo" ]]; then
+        pb_repo="$default_pb_repo"
+    fi
+
+    git ls-remote --exit-code "$pb_repo" &> /dev/null
+
+    if [ "$?" -ne 0 ]; then
+        echo "Error: Unable to join the repo '$pb_repo'"
+        exit 1;
+    fi
+
+    # Save the repo
+    git config --add pb.repo "$pb_repo"
+
+else
+    pb_repo=$(git config pb.repo)
+fi
