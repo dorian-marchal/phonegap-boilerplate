c1ae8d9cce870670e61da469ba0a8420c9024e66
dorian-marchal <marchal.dorian@gmail.com>
2015-04-10 23:47:54 +0200

    Fix router require (bis)

--- www/js/core/views/AppView.js
+++ www/js/core/views/AppView.js
@@ -4,11 +4,11 @@
  * The general code of the views goes here.
  */
  define([
+    'globals',
     'jquery',
     'underscore',
     'backbone',
-    'app/router',
-], function ($, _, Backbone, router) {
+], function (globals, $, _, Backbone) {
     'use strict';
 
     return Backbone.View.extend({
@@ -32,7 +32,7 @@
                     event.preventDefault();
                 },
                 'click [data-route]' : function(event) {
-                    router.navigate($(event.currentTarget).attr('data-route'), true);
+                    globals.router.navigate($(event.currentTarget).attr('data-route'), true);
                     event.preventDefault();
                 }
             });
