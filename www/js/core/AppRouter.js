ff8a09f8f0cde0ab006c5db9ef8a20125e77b3da
dorian-marchal <marchal.dorian@gmail.com>
2015-04-12 00:35:02 +0200

    Automatize routes handling

--- www/js/core/AppRouter.js
+++ www/js/core/AppRouter.js
@@ -9,6 +9,65 @@ define([
 
     return Backbone.Router.extend({
 
+        controllers: {},
+
+        routes: {
+            ':controller(/:action)(/*params)': 'routeToController',
+        },
+
+        initialize: function() {
+
+            var controller = null;
+
+            // Load all the controllers
+            for (var i in this.uses) {
+                controller = new this.uses[i]();
+                this.controllers[controller.route] = controller;
+            }
+        },
+
+        routeToController: function(controller, action, stringParams) {
+
+            var params = this.extractParams(stringParams);
+
+            // index is the default action
+            if (!action) {
+                action = 'index';
+            }
+
+            // If the controller/action couple exists, we call controller.action(param1, param2, ...)
+            // Controller functions that begins with "_" are not actions
+            if (typeof this.controllers[controller] !== 'undefined' &&
+                action.charAt(0) !== '_' &&
+                typeof this.controllers[controller][action] === 'function') {
+                this.controllers[controller][action].apply(this.controllers[controller], params);
+            }
+        },
+
+        /**
+         * Convert stringParams string in array of parameters
+         * @param  {String} stringParams
+         * @return {Array}
+         */
+        extractParams: function(stringParams) {
+            var params = [];
+
+            if (stringParams) {
+                var arrayParams = stringParams.split('/');
+
+                for (var i in arrayParams) {
+                    var param = arrayParams[i];
+
+                    // We don't keep empty params
+                    if (param) {
+                        params.push(param);
+                    }
+                }
+            }
+
+            return params;
+        },
+
         setSlider: function(slider) {
             this.slider = slider;
         },
