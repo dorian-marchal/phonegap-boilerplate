5af7511ab85da0f15d7333927dcdd2d5607eafd9
dorian-marchal <marchal.dorian@gmail.com>
2015-04-12 21:00:45 +0200

    Allow custom routing

--- www/js/core/AppRouter.js
+++ www/js/core/AppRouter.js
@@ -12,36 +12,54 @@ define([
         controllers: {},
 
         routes: {
-            ':controller(/:action)(/*params)': 'routeToController',
+            '(:controller)(/:action)(/*params)': 'routeAction',
         },
 
         initialize: function() {
 
             var controller = null;
 
+            // Add the child routes
+            for (var key in this.additionalRoutes) {
+                this.route(key, this.additionalRoutes[key]);
+            }
+
             // Load all the controllers
             for (var i in this.uses) {
                 controller = new this.uses[i]();
                 this.controllers[controller.route] = controller;
             }
+
         },
 
-        routeToController: function(controller, action, stringParams) {
+        routeAction: function(controller, action, stringParams) {
 
             var params = this.extractParams(stringParams);
 
             // index is the default action
-            if (!action) {
-                action = 'index';
-            }
+            action = action || 'index';
 
             // If the controller/action couple exists, we call controller.action(param1, param2, ...)
             // Controller functions that begins with "_" are not actions
             if (typeof this.controllers[controller] !== 'undefined' &&
                 action.charAt(0) !== '_' &&
                 typeof this.controllers[controller][action] === 'function') {
-                this.controllers[controller][action].apply(this.controllers[controller], params);
+                this.callAction(controller, action, params);
             }
+            else {
+                this.unknownRoute(controller + '.' + action + '(' + params.join(', ') + ')');
+            }
+        },
+
+        callAction: function(controller, action, params) {
+            this.controllers[controller][action].apply(this.controllers[controller], params);
+        },
+
+        /**
+         * Called when no matching routes is found (can be implemented by the child)
+         */
+        unknownRoute: function(other) {
+            console.error('Unknown route : ' + other);
         },
 
         /**
