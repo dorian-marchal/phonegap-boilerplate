95a01929d26ca6b01915e62ebb877a45ad9bcb62
dorian-marchal <marchal.dorian@gmail.com>
2015-04-12 17:51:15 +0200

    Improve Controller constructor

--- www/js/core/AppController.js
+++ www/js/core/AppController.js
@@ -10,17 +10,35 @@ define([
 
     var AppController = function() {};
 
-    AppController.prototype.slider = new PageSlider($('body'));
+    AppController.prototype = {
 
-    /**
-     * Load a PageView in the given layout.
-     */
-    AppController.prototype.loadPage = function (layout, pageView) {
-        layout.setOptions(pageView.layoutOptions);
-        layout.setContentView(pageView);
-        this.slider.slidePage(layout.render().$el);
-        layout.delegateEvents();
-        pageView.delegateEvents();
+        slider: new PageSlider($('body')),
+
+        /**
+         * Load a PageView in the given layout.
+         */
+        loadPage: function (layout, pageView) {
+            layout.setOptions(pageView.layoutOptions);
+            layout.setContentView(pageView);
+            this.slider.slidePage(layout.render().$el);
+            layout.delegateEvents();
+            pageView.delegateEvents();
+        },
+
+    };
+
+    AppController.extend = function(newProps) {
+
+        newProps.initialize = newProps.initialize || function() {};
+
+        var Child = function() {
+            AppController.apply(this, arguments);
+            newProps.initialize.apply(this, arguments);
+        };
+        Child.prototype = $.extend(true, {}, AppController.prototype, newProps);
+        Child.prototype.constructor = Child;
+
+        return Child;
     };
 
     return AppController;
