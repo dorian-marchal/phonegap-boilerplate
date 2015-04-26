38c0cb12d6da6841bee0ec7d47b3fc26f9dc87a4
dorian-marchal <marchal.dorian@gmail.com>
2015-04-26 23:12:28 +0200

    Add a gulpfile with livereload config

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var gulp  = require('gulp');
+
+var tinylr;
+var lrPort = 35729;
+
+var lrWatchPaths = [
+    'www/css/*.css',
+    'www/js/app/templates/*.html',
+    'www/js/app/views/*.js',
+];
+
+function notifyLiveReload(event) {
+    var fileName = require('path').relative(__dirname, event.path);
+
+    tinylr.changed({
+        body: {
+          files: [ fileName ]
+        }
+    });
+}
+
+gulp.task('livereload', ['watch'], function() {
+    tinylr = require('tiny-lr')();
+    tinylr.listen(lrPort);
+
+    for (var i in lrWatchPaths) {
+        gulp.watch(lrWatchPaths[i], notifyLiveReload);
+    }
+});
