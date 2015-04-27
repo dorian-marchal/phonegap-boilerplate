01898b153234fcc5c046d0a9411d9b4f0b8bc7ae
Dorian MARCHAL <marchal.dorian@gmail.com>
2015-04-27 09:41:22 +0200

    Add sass/compass live with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,18 @@
 
 var gulp  = require('gulp');
 
+var webrootPath = 'www';
+var cssPath = webrootPath + '/css';
+var sassPath = cssPath + '/sass';
+var jsPath = webrootPath + '/js';
+
 var tinylr;
 var lrPort = 35729;
 
 var lrWatchPaths = [
-    'www/css/*.css',
-    'www/js/app/templates/*.html',
-    'www/js/app/views/*.js',
+    cssPath + '/*.css',
+    jsPath + '/app/templates/*.html',
+    jsPath + '/app/views/*.js',
 ];
 
 function notifyLiveReload(event) {
@@ -21,11 +26,30 @@ function notifyLiveReload(event) {
     });
 }
 
-gulp.task('livereload', function() {
+gulp.task('live', ['livecompass', 'livereload']);
+
+gulp.task('livereload', function () {
     tinylr = require('tiny-lr')();
     tinylr.listen(lrPort);
 
     for (var i in lrWatchPaths) {
         gulp.watch(lrWatchPaths[i], notifyLiveReload);
     }
+});
+
+gulp.task('livecompass', function () {
+    gulp.watch(sassPath + '/*.scss', ['compass']);
+});
+
+gulp.task('compass', function () {
+
+    var compass = require('gulp-compass');
+
+    gulp.src(sassPath + '/*.scss')
+        .pipe(compass({
+            config_file: sassPath + '/config.rb',
+            css: cssPath,
+            sass: sassPath,
+        }))
+        .pipe(gulp.dest(cssPath));
 });
