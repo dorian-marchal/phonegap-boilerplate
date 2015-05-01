13069b64b1b6c23840a69297f0cfdddd8a283a2b
dorian-marchal <marchal.dorian@gmail.com>
2015-05-01 18:52:48 +0200

    Revert keyframe animation

--- www/js/core/utils/PageSlider.js
+++ www/js/core/utils/PageSlider.js
@@ -10,14 +10,8 @@ define([
 
     return function PageSlider(container) {
 
-        var animationsEnabled = true;
-
         var $currentPage,
             stateHistory = [];
-
-        this.setAnimationsEnabled = function(enableAnimation) {
-            animationsEnabled = enableAnimation;
-        };
 
         this.back = function () {
             location.hash = stateHistory[stateHistory.length - 2];
@@ -25,23 +19,23 @@ define([
 
         // Use this function if you want PageSlider to automatically determine
         // the sliding direction based on the state history.
-        // afterTransition function is called when the transition ends
-        this.slidePage = function ($newPage, options) {
+        // onTransitionEndCallback function is called when the transition ends
+        this.slidePage = function ($newPage, onTransitionEndCallback) {
 
-            var l = stateHistory.length;
-            var state = location.hash;
+            var l = stateHistory.length,
+                state = window.location.hash;
 
             if (l === 0) {
                 stateHistory.push(state);
-                this.slidePageFrom($newPage, null, options);
+                this.slidePageFrom($newPage, null, onTransitionEndCallback);
                 return;
             }
             if (state === stateHistory[l - 2]) {
                 stateHistory.pop();
-                this.slidePageFrom($newPage, 'left', options);
+                this.slidePageFrom($newPage, 'page-left', onTransitionEndCallback);
             } else {
                 stateHistory.push(state);
-                this.slidePageFrom($newPage, 'right', options);
+                this.slidePageFrom($newPage, 'page-right', onTransitionEndCallback);
             }
 
         };
@@ -50,46 +44,38 @@ define([
          * Use this function directly if you want to control the sliding direction outside PageSlider
          * @param  {$} $newPage The new page to slide in
          * @param  {String} from Origin of the slide ('page-left', 'page-right', or null)
-         * @param  {function} options
-         *  beforeTransition: Called before the transition, after the page is added
-         *                    to the DOM.
-         *   afterTransition: Called when the slide end
+         * @param  {function} onTransitionEndCallback Called when the slide end
          *                    or immediately if there is no transition.
          *                    A boolean is passed to the callback : true if we just slide
          *                    in the very first page.
          *
          */
-        this.slidePageFrom = function ($newPage, from, options) {
+        this.slidePageFrom = function ($newPage, from, onTransitionEndCallback) {
 
-            options.beforeTransition = options.beforeTransition || $.noop;
-            options.afterTransition = options.afterTransition || $.noop;
+            onTransitionEndCallback = onTransitionEndCallback || $.noop;
 
             // Current page must be removed after the transition
             var $oldPage = $currentPage;
             var firstSlide = !$oldPage;
-            var to = (from === 'left' ? 'right' : 'left');
 
-            container.append($newPage.addClass('page'));
-            options.beforeTransition();
+            container.append($newPage);
+
+            $newPage.addClass('page');
 
             // First loaded page (no old page) or no transition
-            if (firstSlide || !from || !animationsEnabled) {
-                // Remove old page if it exists
-                if ($oldPage) {
-                    $oldPage.remove();
-                }
+            if (firstSlide || !from) {
 
-                $newPage.addClass('page-center');
+                $newPage.addClass('page-center no-transition');
 
                 $currentPage = $newPage;
 
                 // We call the transition end callback anyway
-                options.afterTransition(firstSlide);
+                onTransitionEndCallback(firstSlide);
                 return;
             }
 
             // Position the page at the starting position of the animation
-            $newPage.addClass('page-' + from);
+            $newPage.addClass(from);
 
             // Shim transitionend if it's not fired
             var shimTransitionEnd = setTimeout(function() {
@@ -106,12 +92,12 @@ define([
 
             // Position the new page and the current page at the ending position of their animation with a transition class indicating the duration of the animation
             $newPage
-                .removeClass('page-left page-right')
-                .addClass('page-center from-' + from);
+                .removeClass('page-left page-right no-transition')
+                .addClass('transition page-center');
 
             $oldPage
-                .removeClass('page-center')
-                .addClass('page-' + to + ' to-' + to);
+                .removeClass('page-center no-transition')
+                .addClass('transition ' + (from === 'page-left' ? 'page-right' : 'page-left'));
 
             $currentPage = $newPage;
 
@@ -125,7 +111,7 @@ define([
                 container[0].offsetWidth;
 
                 clearTimeout(shimTransitionEnd);
-                options.afterTransition(false);
+                onTransitionEndCallback(false);
             };
         };
 
