bceacaeec0c3b7d6ca5981335fef17f323c66bbc
dorian-marchal <marchal.dorian@gmail.com>
2015-07-19 20:01:02 +0200

    Remove obsolete dev files

--- dev-scripts/pb-config.sh
+++
@@ -1,7 +0,0 @@
-# Default remote pb repo to pull from
-default_pb_repo="git@github.com:dorian-marchal/phonegap-boilerplate.git"
-
-# Default branch to pull from on pb-core
-# Be careful if you change this in middle of a project.
-# Your pb-core branch should never be ahead of this branch;
-pb_branch="dev"
